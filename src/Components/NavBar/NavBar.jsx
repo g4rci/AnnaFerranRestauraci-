86d8a0dfe763938b95934bfeb6d5044bce9017f3
M.A.Garcia <m.angel.gf04.com>
2021-02-18 16:03:00 +0100

    .env

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -7,7 +7,7 @@ export default function NavBar() {
     <div >
       <div>
       <Navbar bg="dark" variant="dark" expand="lg">
-      <Navbar.Brand href="/">AnnaFerran Restauració</Navbar.Brand>
+      <Navbar.Brand href="/">Anna Ferran Restauració</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
